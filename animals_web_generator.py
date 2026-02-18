76731cbafe4db0384e2177c62ec0618432df08b5
Malte Neemann <malteneemann129@gmail.com>
2026-02-18 17:30:13 +0100

    Formatted the code. Extra function for structuring the html/css for each animal seperaly. added a main functionn

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -6,70 +6,65 @@ def load_data(file_path):
     with open(file_path, "r", encoding="utf-8") as handle:
         return json.load(handle)
 
-#Get the JSON Information about the animals
-animals_data = load_data("animals_data.json")
-
 
 def read_html(html_page):
     """Reads the template"""
-    with open(html_page, "r") as website:
+    with open(html_page, "r", encoding="utf-8") as website:
         return website.read()
 
-#Read the file and save the Template
-html = read_html("animals_template.html")
 
-
-def get_animals_information(data):
-    """Loops through the data and shows picked Informations about the animals.
-    If information is missing, we skip the missing part"""
+def serialize_animal(animal_obj):
+    """Serializes a single animal object to HTML"""
     output = ""
-    for animal in data:
-        #Open new list element for every animal
-        output += '<li class="cards__item">'
 
-        #Title with the name
-        output += '<div class="card__title">'
-        output += f"{animal['name']}<br/>\n"
-        output += '</div>'
+    #Open new list element for animal
+    output += '<li class="cards__item">'
 
-        #Text-element for the datails
-        output += '<p class="card__text">'
+    #Title for the name
+    output += '<div class="card__title">'
+    output += f"{animal_obj['name']}<br/>\n"
+    output += '</div>'
 
-        #Check for Diet
-        try:
-            if animal['characteristics']['diet']:
-                output += '<strong>Diet:</strong> '
-                output += f"{animal['characteristics']['diet']}<br/>\n"
-        except KeyError:
-            pass
+    #Text element for the bbody
+    output += '<p class="card__text">'
 
-        #Check for Location
-        try:
-            if animal['locations'][0]:
-                output += '<strong>Location:</strong> '
-                output += f"{animal['locations'][0]}<br/>\n"
-        except (KeyError, IndexError):
-            pass
+    #Check diet
+    try:
+        if animal_obj['characteristics']['diet']:
+            output += '<strong>Diet:</strong> '
+            output += f"{animal_obj['characteristics']['diet']}<br/>\n"
+    except KeyError:
+        pass
 
-        #Check for Type
-        try:
-            if animal['characteristics']['type']:
-                output += '<strong>Type:</strong> '
-                output += f"{animal['characteristics']['type']}<br/>\n"
-        except KeyError:
-            pass
+    #Check location
+    try:
+        if animal_obj['locations'][0]:
+            output += '<strong>Location:</strong> '
+            output += f"{animal_obj['locations'][0]}<br/>\n"
+    except (KeyError, IndexError):
+        pass
 
-        #Close Text and List element
-        output += '</p>'
-        output += '</li>'
+    #Check type
+    try:
+        if animal_obj['characteristics']['type']:
+            output += '<strong>Type:</strong> '
+            output += f"{animal_obj['characteristics']['type']}<br/>\n"
+    except KeyError:
+        pass
+
+    #Close text and list element
+    output += '</p>'
+    output += '</li>'
 
     return output
 
-#Get the Information we need as a string:
-data_of_all_animals = get_animals_information(animals_data)
 
-#Replace the new Information in the template:
-new_html = html.replace("__REPLACE_ANIMALS_INFO__", data_of_all_animals)
+def get_animals_information(data):
+    """Loops through the data and serializes all animals"""
+    output = ""
+    for animal_obj in data:
+        output += serialize_animal(animal_obj)
+    return output
 
 
 def write_new_html(file_name, content):
@@ -77,5 +72,14 @@ def write_new_html(file_name, content):
     with open(file_name, "w", encoding="utf-8") as html_website:
         html_website.write(content)
 
-#Create a new html file. We need to give the new name and the adapted Template
-write_new_html("animals.html", new_html)
+
+def main():
+    animals_data = load_data("animals_data.json")
+    html = read_html("animals_template.html")
+    data_of_all_animals = get_animals_information(animals_data)
+    new_html = html.replace("__REPLACE_ANIMALS_INFO__", data_of_all_animals)
+    write_new_html("animals.html", new_html)
+
+
+if __name__ == "__main__":
+    main()
